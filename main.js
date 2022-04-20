cd27b7b71db9ec62fc8eb855f55bda75fd1752e9
nazonasirkey <abdinasirmaalim23@gmail.com>
2022-04-20 17:30:00 +0300

    it will add functionality

--- main.js
+++ main.js
@@ -2,5 +2,5 @@ let form = document.querySelector(".form")
 let username = form.username
 form.addEventListener("submit", (e) => {
 e.preventDefault()
-alert("Hello " + username.value + "! thank you for contacting Ajab solution limited, we recieved your masssage .contact us 0722283195 for more information. Ramadhan mubarak")
+alert("Hello " + username.value + "! thank you for contacting Ajabfix limited, we recieved your message. for more information contact us 0722283195. Ramadhan mubarak")
  });
